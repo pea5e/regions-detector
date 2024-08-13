37de5f1abbb611f5f34a19856e577099c73ae716
Salim Alaoui <107775162+pea5e@users.noreply.github.com>
2024-08-13 04:20:09 +0100

    Update topojson.py

--- topojson.py
+++ topojson.py
@@ -86,6 +86,7 @@ print(transform)
 coord = (34.614845, -5.288887)
 coord = (35.549780, -5.574435)
 coord = (34.582470, -5.316348)
+coord = (34.796643, -5.289176)
 
 x = coord[1] - transform["translate"][0];
 y = coord[0] - transform["translate"][1];
@@ -125,16 +126,35 @@ print(xperScale,yperScale)
 #         cpath[0] = cpath[0]+dirs[0]
 #         cpath[1] = cpath[1]+dirs[1]
 
+# l = list()
+
+# for arcIndex,arc in enumerate(regions[0]["arcs"][0]):
+#     print(arc)
+#     x = arc if arc >= 0 else arc*-1-1
+#     cpath = [js["arcs"][x][0][0],js["arcs"][x][0][1]]
+#     for dirIndex,dirs in enumerate(js["arcs"][x][1:],start=1):
+#         if (cpath[1]<=yperScale and cpath[1]+dirs[1]>=yperScale) or (cpath[1]>=yperScale and cpath[1]+dirs[1]<=yperScale):
+#             # print(cpath,dirs)
+#             l.append([[cpath[0]+(abs(yperScale-cpath[1])/dirs[1])*dirs[0],yperScale],cpath,arcIndex,dirIndex,dirs,arc])
+#         cpath[0] = cpath[0]+dirs[0]
+#         cpath[1] = cpath[1]+dirs[1]
+
+# print(l)
+
 l = list()
 
 for arcIndex,arc in enumerate(regions[0]["arcs"][0]):
-    print(arc)
     x = arc if arc >= 0 else arc*-1-1
     cpath = [js["arcs"][x][0][0],js["arcs"][x][0][1]]
     for dirIndex,dirs in enumerate(js["arcs"][x][1:],start=1):
         if (cpath[1]<=yperScale and cpath[1]+dirs[1]>=yperScale) or (cpath[1]>=yperScale and cpath[1]+dirs[1]<=yperScale):
-            # print(cpath,dirs)
-            l.append([cpath,arcIndex,dirIndex])
+            l.append([[cpath[0]+(abs(yperScale-cpath[1])/dirs[1])*dirs[0],yperScale],cpath,arcIndex,dirIndex,dirs,arc])
+            if len(l)==2:
+                print(l)
+                if ((l[0][0][0]<=xperScale and l[1][0][0]>=xperScale) or (l[0][0][0]>=xperScale and l[1][0][0]<=xperScale)) and (( l[0][-1]==l[1][-1] and l[1][-1]>=0 ) or l[0][-1]!=l[1][-1]) : 
+                    print("included\n")
+                    exit()
+                l.clear()
         cpath[0] = cpath[0]+dirs[0]
         cpath[1] = cpath[1]+dirs[1]
 
